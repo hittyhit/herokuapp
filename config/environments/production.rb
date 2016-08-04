593f6f10dfb088558a61dfa63bb53e000d60edd6
hittyhit <hitty@i.ua>
2016-08-04 14:35:26 +0300

    action mailer to deliver via email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
@@ -13,20 +13,18 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'http://peaceful-mesa-78231.herokuapp.com/' }
-
-  ActionMailer::Base.delivery_method = :file
-  ActionMailer::Base.perform_deliveries = true
-
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
 end
-# На случай деплоя на Heroku
-#  ActionMailer::Base.smtp_settings = {
-#    :address              => 'smtp.sendgrid.net',
-#    :port                 => '587',
-#    :domain               => 'heroku.com',
-#    :user_name            => ENV['USERNAME'],
-#    :password             => ENV['PASSWORD'],
-# :authentication       => "plain",
-#  :enable_starttls_auto => true
-#  }
-#end
+
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => 'smtp.sendgrid.net',
+    :port                 => '587',
+    :authentication => :plain,
+    :user_name            => 'litest',
+    :password             => 'litest123123',
+    :domain               => 'peaceful-mesa-78231.herokuapp.com',
+  :enable_starttls_auto => true
+  }
+
